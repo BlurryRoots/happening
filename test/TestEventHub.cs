366147ba1307c41376bc8ad2dba9887ee5122d45
blurryroots <blurryroots@posteo.de>
2016-08-25 17:43:18 +0200

    added test cases for fire method

---
+++ test/TestEventHub.cs
@@ -0,0 +1,110 @@
+﻿namespace BlurryRoots.Happening.Test {
+
+    using NUnit.Framework;
+    using BlurryRoots.Happening;
+
+    [TestFixture]
+    public class TestEventHub {
+
+        [Test]
+        public void SubscribeRaiseAndDispatch () {
+            var eventHub = new EventHub ();
+            
+            var hasBeenCalled = 0;
+            EventCallback<TestEventTypeSimple> callback = 
+                (TestEventTypeSimple e) => {
+                    ++hasBeenCalled;
+                };
+
+            eventHub.Subscribe (callback);
+
+            eventHub.Raise (new TestEventTypeSimple () {
+                id = 1337,
+                name = "Großherzug Hans von Wurst"
+            });
+
+            Assert.AreEqual (0, hasBeenCalled,
+                "Delegate should not have been called at this point!"
+            );
+
+            eventHub.DispatchAllRaisedEvents ();
+
+            Assert.AreEqual (1, hasBeenCalled,
+                "Delegate callback has not been called!"
+            );
+        }
+
+        [Test]
+        public void SubscribeAndFire () {
+            var eventHub = new EventHub ();
+
+            var hasBeenCalled = 0;
+            EventCallback<TestEventTypeSimple> callback =
+                (TestEventTypeSimple e) => {
+                    ++hasBeenCalled;
+                };
+
+            eventHub.Subscribe (callback);
+
+            eventHub.Fire (new TestEventTypeSimple () {
+                id = 1337,
+                name = "Großherzug Hans von Wurst"
+            });
+
+            Assert.AreEqual (1, hasBeenCalled,
+                "Delegate callback has not been called!"
+            );
+        }
+
+        [Test]
+        public void SubscribeRaiseFireAndUnsubscribe () {
+            var eventHub = new EventHub ();
+
+            var hasBeenCalled = 0;
+            EventCallback<TestEventTypeSimple> callback =
+                (TestEventTypeSimple e) => {
+                    ++hasBeenCalled;
+                };
+
+            eventHub.Subscribe (callback);
+
+            eventHub.Raise (new TestEventTypeSimple () {
+                id = 1337,
+                name = "Großherzug Hans von Wurst"
+            });
+
+            Assert.AreEqual (0, hasBeenCalled,
+                "Delegate should not have been called at this point!"
+            );
+
+            eventHub.DispatchAllRaisedEvents ();
+
+            Assert.AreEqual (1, hasBeenCalled,
+                "Delegate callback has not been called!"
+            );
+
+            eventHub.Fire (new TestEventTypeSimple () {
+                id = 1337,
+                name = "Großherzug Hans von Wurst"
+            });
+
+            Assert.AreEqual (2, hasBeenCalled,
+                "Delegate callback has not been called!"
+            );
+
+            eventHub.Unsubscribe (callback);
+
+            eventHub.Fire (new TestEventTypeSimple () {
+                id = 1337,
+                name = "Großherzug Hans von Wurst"
+            });
+
+            Assert.AreEqual (2, hasBeenCalled,
+                "Delegate callback has not been called!"
+            );
+
+        }
+
+    }
+
+}
